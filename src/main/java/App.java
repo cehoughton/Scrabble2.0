0e06fee9e58f08d015852c5ff3c1368dadbb40ac
Brad Pizzimenti and Chris Houghton <brad@gennakersystems.com>
2016-02-09 09:37:38 -0800

    add first spec passing

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+import static spark.Spark.*;
+
+public class App {
+  public static void main(String[] args) {}
+  public Integer scrabbleScore(String inputWord) {
+    Integer score = 0;
+    char[] charWord = inputWord.toCharArray();
+    return 1;
+
+  }
+}
