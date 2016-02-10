4e30ed0e9ae6f552bdfd14b819a1ba30e606f299
Brad Pizzimenti and Chris Houghton <brad@gennakersystems.com>
2016-02-09 16:52:09 -0800

    add spark site functionality

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,33 @@
-import static spark.Spark.*;
 import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
 
 public class App {
-  public static void main(String[] args) {}
-  public Integer scrabbleScore(String inputWord) {
+  public static void main(String[] args) {
+
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+          HashMap model = new HashMap();
+          model.put("template", "templates/input.vtl");
+          return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+    get("/output", (request, response) -> {
+          HashMap model = new HashMap();
+          model.put("template", "templates/output.vtl");
+          String word = request.queryParams("word");
+          int wordScore = scrabbleScore(word);
+          model.put("wordScore", wordScore);
+          model.put("word", word);
+          return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+      }
+
+
+
+  public static Integer scrabbleScore(String inputWord) {
     int score = 0;
     String inputWordLowercase = inputWord.toLowerCase();
     String[] inputWordArray = inputWordLowercase.split("");
